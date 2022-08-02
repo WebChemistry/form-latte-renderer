25c9fe6ce020b0114ae6b9e6e8c20fca8a8cd6a6
MartkCz <martk494@gmail.com>
2022-08-02 18:45:03 +0200

    latte 3

--- src/LatteFormRenderer.php
+++ src/LatteFormRenderer.php
@@ -5,6 +5,7 @@ namespace WebChemistry\FormLatteRenderer;
 use Latte\Engine;
 use Nette\Bridges\ApplicationLatte\LatteFactory;
 use Nette\Bridges\FormsLatte\FormMacros;
+use Nette\Bridges\FormsLatte\FormsExtension;
 use Nette\Forms\Form;
 use Nette\Forms\FormRenderer;
 use WebChemistry\FormLatteRenderer\Template\LatteFormTemplate;
@@ -25,9 +26,13 @@ class LatteFormRenderer implements FormRenderer
 		$this->object->form = $form;
 
 		$engine = $this->latteFactory->create();
-		$engine->onCompile[] = function (Engine $engine): void {
-			FormMacros::install($engine->getCompiler());
-		};
+		if (version_compare(Engine::VERSION, '3', '<')) {
+			$engine->onCompile[] = function (Engine $engine): void {
+				FormMacros::install($engine->getCompiler());
+			};
+		} else {
+			$engine->addExtension(new FormsExtension());
+		}
 
 		return $engine->renderToString($this->template, $this->object);
 	}
